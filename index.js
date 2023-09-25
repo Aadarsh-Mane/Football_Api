686301f78020ef654eda700cb05cb574ac0cbd17
Aadarsh Mane <onlyaddy68@gmail.com>
2023-09-25 19:48:11 +0530

    added temporary

--- index.js
+++ index.js
@@ -9,6 +9,57 @@ import bettingroutes from "./routes/bettingevents.js";
 const app = express();
 app.use("/players", eventRoutes);
 app.use("/betting", bettingroutes);
+const newspapers = [
+  {
+    name: 'economictimes',
+      address: 'https://economictimes.indiatimes.com/topic/wool-industry/news',
+      base: 'https://economictimes.indiatimes.com/topic/wool-industry/news'
+  },
+  {
+    name: 'fibre2fashion',
+    address: 'https://www.fibre2fashion.com/news/yarn-news',
+    base: ''
+  },
+  
+]
+const woolTerms=[
+  'wool',
+  'yarn'
+]
+const articles = []
+const duplicatearticles = []
+
+newspapers.forEach(newspaper => {
+  axios.get(newspaper.address)
+  .then(response => {
+    const html = response.data
+    const $ = cheerio.load(html)
+    woolTerms.forEach((term1) => {
+      $(`a:contains(${term1})`, html).each(function () {
+        
+        const anchor = $(this);
+        const headLine = anchor.text();
+        const url = anchor.attr('href');
+        const desc = anchor.next('td').text();
+         
+         
+        if(!duplicatearticles.includes(headLine)){
+          
+           articles.push({
+             headLine,
+            
+             source: newspaper.name,
+             url: newspaper.base + url,
+            })
+            duplicatearticles.push(headLine) 
+          }
+        })
+      })
+    })
+  })
+  app.get("/wool", (req, res) => {
+    res.json(articles)
+  });
 app.get("/", (req, res) => {
   res.json("welcome to  Football API ");
 });
